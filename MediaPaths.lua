6f94127d0f5a040f8537963ef4d0e37edce4d08d
r0b <robdeflop96@gmail.com>
2025-03-02 16:08:51 +0100

    Init Commit

---
+++ MediaPaths.lua
@@ -0,0 +1,12 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+local MediaType_Font = LSM.MediaType.FONT
+local MediaType_Statusbar = LSM.MediaType.STATUSBAR
+local MediaType_Border = LSM.MediaType.BORDER
+local MediaType_Background = LSM.MediaType.BACKGROUND
+local MediaType_Sound = LSM.MediaType.SOUND
+
+
+--- Sounds
+LSM:Register(MediaType_Sound, "|cffe8c723<NPNR>|r Frontal",
+    "Interface\\AddOns\\SharedMedia_NPNR\\Media\\Sounds\\Frontal.ogg")
